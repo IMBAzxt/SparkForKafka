4cece5bb98063ef0a19556a7d611b9dd348cc8fc
zhengxt <364619221@qq.com>
2018-09-02 15:32:45 +0800

    add suport for offset

--- src/main/java/com/zhengxuetao/SparkForKafka.java
+++ src/main/java/com/zhengxuetao/SparkForKafka.java
@@ -4,17 +4,21 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.Time;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaInputDStream;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
-import org.apache.spark.streaming.kafka010.ConsumerStrategies;
-import org.apache.spark.streaming.kafka010.KafkaUtils;
-import org.apache.spark.streaming.kafka010.LocationStrategies;
+import org.apache.spark.streaming.kafka010.*;
 import scala.Tuple2;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author zhengxt
@@ -38,6 +42,38 @@ public class SparkForKafka {
 
         JavaDStream<String> lines = messages.map(ConsumerRecord::value);
         lines.print();
+        return jssc;
+    }
+
+    public  JavaStreamingContext createStreamingContextForKafkaSaveOffset(String nodeList, String group, String topic) {
+        SparkConf sc = new SparkConf().setAppName("SparkForJava").setMaster("local[2]");
+        JavaStreamingContext jssc = new JavaStreamingContext(sc, Durations.seconds(5));
+
+        Set<String> topicsSet = new HashSet<>(Arrays.asList(topic));
+        Map<String, Object> kafkaParams = new HashMap<>();
+        kafkaParams.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, nodeList);
+        kafkaParams.put(ConsumerConfig.GROUP_ID_CONFIG, group);
+        kafkaParams.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        kafkaParams.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        JavaInputDStream<ConsumerRecord<String, String>> messages = KafkaUtils.createDirectStream(
+                jssc,
+                LocationStrategies.PreferConsistent(),
+                ConsumerStrategies.Subscribe(topicsSet, kafkaParams));
+
+        final AtomicReference<OffsetRange[]> offsetRanges = new AtomicReference<>();
+        messages.transform((Function<JavaRDD<ConsumerRecord<String, String>>, JavaRDD<ConsumerRecord<String, String>>>) rdd -> {
+            OffsetRange[] offsets = ((HasOffsetRanges) rdd.rdd()).offsetRanges();
+            offsetRanges.set(offsets);
+            return rdd;
+        }).foreachRDD(rdd->{
+            for (OffsetRange o : offsetRanges.get()) {
+                System.out.println(
+                        o.topic() + " " + o.partition() + " " + o.fromOffset() + " " + o.untilOffset()
+                );
+            }
+            rdd.foreach(line -> System.out.println(line));
+        });
         return jssc;
     }
 }
