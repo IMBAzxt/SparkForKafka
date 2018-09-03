aa3001569617982891d276f77830a6250bc7de68
zhengxt <364619221@qq.com>
2018-09-03 19:07:05 +0800

    11

--- src/main/java/com/zhengxuetao/SparkForJavaMain.java
+++ src/main/java/com/zhengxuetao/SparkForJavaMain.java
@@ -1,14 +1,14 @@
 package com.zhengxuetao;
 
 import org.apache.spark.api.java.function.Function0;
-import org.apache.spark.streaming.api.java.*;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
 public class SparkForJavaMain {
 
     public static void main(String[] args) throws InterruptedException {
         String checkpointPath = "hdfs://namenode:8020/data/checkpointPath";
-        String nodeList = "dn1:9092,dn2:9092,dn3:9092,nn:9092";
-        String group ="java";
+        String nodeList = "dn1:9092,dn3:9092,nn:9092";
+        String group = "java11";
         String topic = "flumetest";
         JavaStreamingContext jssc = JavaStreamingContext.getOrCreate(
                 checkpointPath,
--- src/main/java/com/zhengxuetao/SparkForKafka.java
+++ src/main/java/com/zhengxuetao/SparkForKafka.java
@@ -12,14 +12,14 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.streaming.Durations;
-import org.apache.spark.streaming.api.java.*;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaPairInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka.HasOffsetRanges;
 import org.apache.spark.streaming.kafka.KafkaUtils;
 import org.apache.spark.streaming.kafka.OffsetRange;
-import org.apache.zookeeper.data.Stat;
-import scala.Tuple2;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
@@ -71,10 +71,10 @@ public class SparkForKafka {
      * @param topic
      * @return
      */
-    public  JavaStreamingContext createStreamingContextForKafkaSaveOffset(String nodeList, String group, String topic) throws Exception {
+    public JavaStreamingContext createStreamingContextForKafkaSaveOffset(String nodeList, String group, String topic) {
         SparkConf sc = new SparkConf().setAppName("SparkForJava").setMaster("local[*]");
         JavaStreamingContext jssc = new JavaStreamingContext(sc, Durations.seconds(5));
-        jssc.sparkContext().setLogLevel("DEBUG");
+        jssc.sparkContext().setLogLevel("ERROR");
         Set<String> topicsSet = new HashSet<>(Arrays.asList(topic));
         Map<String, String> kafkaParams = new HashMap<>();
         kafkaParams.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, nodeList);
@@ -90,7 +90,13 @@ public class SparkForKafka {
                 .build();
         curatorFramework.start();
         String offsetPath = "/consumers/" + group + "/offsets/" + topic;
-        List<String> list = curatorFramework.getChildren().forPath(offsetPath);
+        List<String> list = null;
+        try {
+            list = curatorFramework.getChildren().forPath(offsetPath);
+        } catch (Exception e) {
+            list = new ArrayList<>();
+        }
+
         JavaPairDStream<String,String> messages = null;
         final AtomicReference<OffsetRange[]> offsetRanges = new AtomicReference<>();
         if(list.isEmpty()){
@@ -102,17 +108,26 @@ public class SparkForKafka {
                     StringDecoder.class,
                     kafkaParams,
                     topicsSet
-            );
+            ).transformToPair((Function<JavaPairRDD<String, String>, JavaPairRDD<String, String>>) rdd -> {
+                OffsetRange[] offsets = ((HasOffsetRanges) rdd.rdd()).offsetRanges();
+                offsetRanges.set(offsets);
+                return rdd;
+            });
         } else {
             Map<TopicAndPartition,Long> fromOffsets = new HashMap<>();
             for(int i=0; i < list.size(); i++){
-                TopicAndPartition topicAndPartition = new TopicAndPartition(group,i);
-                byte[] value = curatorFramework.getData().forPath(offsetPath + "/" + i);
-                fromOffsets.put(topicAndPartition,Long.parseLong(new String(value)));
+                TopicAndPartition topicAndPartition = new TopicAndPartition(topic, i);
+                byte[] value = new byte[0];
+                try {
+                    value = curatorFramework.getData().forPath(offsetPath + "/" + i);
+                    fromOffsets.put(topicAndPartition, Long.parseLong(new String(value)));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
             Class<MessageAndMetadata<String,String>> streamClass =
                     (Class<MessageAndMetadata<String,String>>) (Class<?>) MessageAndMetadata.class;
-            messages = KafkaUtils.createDirectStream(
+            KafkaUtils.createDirectStream(
                     jssc,
                     String.class,
                     String.class,
@@ -122,37 +137,42 @@ public class SparkForKafka {
                     kafkaParams,
                     fromOffsets,
                     (Function<MessageAndMetadata<String, String>, MessageAndMetadata<String, String>>) v1 -> v1
-            ).mapToPair((PairFunction<MessageAndMetadata<String, String>, String, String>) s -> new Tuple2<>(s.topic(),s.message()));
+            ).transform(new Function<JavaRDD<MessageAndMetadata<String, String>>, JavaRDD<MessageAndMetadata<String, String>>>() {
+                @Override
+                public JavaRDD<MessageAndMetadata<String, String>> call(JavaRDD<MessageAndMetadata<String, String>> rdd) throws Exception {
+                    OffsetRange[] offsets = ((HasOffsetRanges) rdd.rdd()).offsetRanges();
+                    offsetRanges.set(offsets);
+                    return rdd;
+                }
+            }).foreachRDD(rdd -> {
+                CuratorFramework cf = CuratorFrameworkFactory
+                        .builder()
+                        .connectString("nn:2181")
+                        .connectionTimeoutMs(1000)
+                        .sessionTimeoutMs(10000)
+                        .retryPolicy(new RetryUntilElapsed(1000, 1000))
+                        .build();
+
+                cf.start();
+                ObjectMapper objectMapper = new ObjectMapper();
+                for (OffsetRange o : offsetRanges.get()) {
+                    System.out.println(
+                            o.topic() + " " + o.partition() + " " + o.fromOffset() + " " + o.untilOffset()
+                    );
+                    final byte[] offsetBytes = objectMapper.writeValueAsBytes(o.untilOffset());
+                    String nodePath = "/consumers/" + group + "/offsets/" + o.topic() + "/" + o.partition();
+                    if (cf.checkExists().forPath(nodePath) != null) {
+                        cf.setData().forPath(nodePath, offsetBytes);
+                    } else {
+                        cf.create().creatingParentsIfNeeded().forPath(nodePath, offsetBytes);
+                    }
+                }
+                rdd.foreach(line -> System.out.println(line));
+                cf.close();
+            });
         }
 
         curatorFramework.close();
-
-
-
-        messages.transformToPair((Function<JavaPairRDD<String, String>, JavaPairRDD<String, String>>) rdd -> {
-            OffsetRange[] offsets = ((HasOffsetRanges) rdd.rdd()).offsetRanges();
-            offsetRanges.set(offsets);
-            return rdd;
-        }).foreachRDD(rdd->{
-
-            curatorFramework.start();
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            for (OffsetRange o : offsetRanges.get()) {
-                System.out.println(
-                        o.topic() + " " + o.partition() + " " + o.fromOffset() + " " + o.untilOffset()
-                );
-                final byte[] offsetBytes = objectMapper.writeValueAsBytes(o.untilOffset());
-                String nodePath = "/consumers/" + group + "/offsets/" + o.topic()+ "/" + o.partition();
-                if(curatorFramework.checkExists().forPath(nodePath) != null){
-                    curatorFramework.setData().forPath(nodePath,offsetBytes);
-                }else{
-                    curatorFramework.create().creatingParentsIfNeeded().forPath(nodePath, offsetBytes);
-                }
-            }
-            curatorFramework.close();
-            rdd.foreach(line -> System.out.println(line));
-        });
         return jssc;
     }
 }
