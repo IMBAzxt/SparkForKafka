7a2cc8a20b20efd95769bf480150489f88156292
zhengxt <364619221@qq.com>
2018-09-04 11:12:06 +0800

    使用ZkClient的方式来读取并保存offset到zookeeper。

--- src/main/java/com/zhengxuetao/SparkForJavaMain.java
+++ src/main/java/com/zhengxuetao/SparkForJavaMain.java
@@ -8,11 +8,12 @@ public class SparkForJavaMain {
     public static void main(String[] args) throws InterruptedException {
         String checkpointPath = "hdfs://namenode:8020/data/checkpointPath";
         String nodeList = "dn1:9092,dn3:9092,nn:9092";
-        String group = "java8";
+        String group = "java7";
         String topic = "flumetest";
         JavaStreamingContext jssc = JavaStreamingContext.getOrCreate(
                 checkpointPath,
-                (Function0<JavaStreamingContext>) () -> new SparkForKafka().createStreamingContextForKafkaSaveOffset(nodeList,group,topic));
+                (Function0<JavaStreamingContext>) () -> new SparkForKafka()
+                        .createStreamingContextForKafkaSaveOffsetByZkClient(nodeList, group, topic));
         jssc.start();
         jssc.awaitTermination();
     }
--- src/main/java/com/zhengxuetao/SparkForKafka.java
+++ src/main/java/com/zhengxuetao/SparkForKafka.java
@@ -8,6 +8,9 @@ import kafka.javaapi.TopicMetadataRequest;
 import kafka.javaapi.consumer.SimpleConsumer;
 import kafka.message.MessageAndMetadata;
 import kafka.serializer.StringDecoder;
+import kafka.utils.ZKGroupTopicDirs;
+import kafka.utils.ZkUtils;
+import org.I0Itec.zkclient.ZkClient;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.RetryUntilElapsed;
@@ -17,7 +20,6 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
-import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaPairInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka.HasOffsetRanges;
@@ -78,7 +80,6 @@ public class SparkForKafka {
         SparkConf sc = new SparkConf().setAppName("SparkForJava").setMaster("local[*]");
         JavaStreamingContext jssc = new JavaStreamingContext(sc, Durations.seconds(5));
         jssc.sparkContext().setLogLevel("ERROR");
-        Set<String> topicsSet = new HashSet<>(Arrays.asList(topic));
         Map<String, String> kafkaParams = new HashMap<>();
         kafkaParams.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, nodeList);
         kafkaParams.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "smallest");
@@ -100,7 +101,6 @@ public class SparkForKafka {
             list = new ArrayList<>();
         }
 
-        JavaPairDStream<String,String> messages = null;
         final AtomicReference<OffsetRange[]> offsetRanges = new AtomicReference<>();
         Map<TopicAndPartition, Long> fromOffsets = new HashMap<>();
 
@@ -172,6 +172,79 @@ public class SparkForKafka {
 
 
         curatorFramework.close();
+        return jssc;
+    }
+
+    public JavaStreamingContext createStreamingContextForKafkaSaveOffsetByZkClient(String nodeList, String group, String topic) {
+        SparkConf sc = new SparkConf().setAppName("SparkForJava").setMaster("local[*]");
+        JavaStreamingContext jssc = new JavaStreamingContext(sc, Durations.seconds(5));
+        jssc.sparkContext().setLogLevel("ERROR");
+        Map<String, String> kafkaParams = new HashMap<>();
+        kafkaParams.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, nodeList);
+        kafkaParams.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "smallest");
+        kafkaParams.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "FALSE");
+        kafkaParams.put(ConsumerConfig.GROUP_ID_CONFIG, group);
+        ZKGroupTopicDirs zkGroupTopicDirs = new ZKGroupTopicDirs(group, topic);
+        ZkClient zkClient = new ZkClient("dn1:2181");
+        int count = zkClient.countChildren(zkGroupTopicDirs.consumerOffsetDir());
+
+        final AtomicReference<OffsetRange[]> offsetRanges = new AtomicReference<>();
+        Map<TopicAndPartition, Long> fromOffsets = new HashMap<>();
+
+        //从SimpleConsumer中获取topic的partition信息
+        TreeMap<Integer, PartitionMetadata> map = findLeader("dn1", 9092, topic);
+        for (Map.Entry<Integer, PartitionMetadata> entry : map.entrySet()) {
+            TopicAndPartition topicAndPartition = new TopicAndPartition(topic, entry.getKey());
+            if (count == 0) {
+                //zookeeper中没有topic的信息，则从0开始
+                fromOffsets.put(topicAndPartition, 0L);
+            } else {
+                try {
+                    //从zookeeper中获取offset信息，并从保存的offset处开始读取
+                    String value = zkClient.readData(zkGroupTopicDirs.consumerOffsetDir() + "/" + entry.getKey());
+                    //"OffsetRange(topic: 'flumetest', partition: 0, range: [38 -> 38])"，获取offset值，range表示from->end
+                    Long offs = Long.parseLong(value.substring(value.indexOf("[") + 1, value.indexOf("->") - 1));
+                    fromOffsets.put(topicAndPartition, offs);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    //如果zookeeper中没有partition的offset，也从0开始。
+                    fromOffsets.put(topicAndPartition, 0L);
+                }
+            }
+        }
+
+
+        Class<MessageAndMetadata<String, String>> streamClass =
+                (Class<MessageAndMetadata<String, String>>) (Class<?>) MessageAndMetadata.class;
+        KafkaUtils.createDirectStream(
+                jssc,
+                String.class,
+                String.class,
+                StringDecoder.class,
+                StringDecoder.class,
+                streamClass,
+                kafkaParams,
+                fromOffsets,
+                (Function<MessageAndMetadata<String, String>, MessageAndMetadata<String, String>>) v1 -> v1
+        ).transform(new Function<JavaRDD<MessageAndMetadata<String, String>>, JavaRDD<MessageAndMetadata<String, String>>>() {
+            @Override
+            public JavaRDD<MessageAndMetadata<String, String>> call(JavaRDD<MessageAndMetadata<String, String>> rdd) throws Exception {
+                OffsetRange[] offsets = ((HasOffsetRanges) rdd.rdd()).offsetRanges();
+                offsetRanges.set(offsets);
+                return rdd;
+            }
+        }).foreachRDD(rdd -> {
+            for (OffsetRange o : offsetRanges.get()) {
+                System.out.println(
+                        o.topic() + " " + o.partition() + " " + o.fromOffset() + " " + o.untilOffset()
+                );
+                String offsetPath = "/consumers/" + group + "/offsets/" + o.topic() + "/" + o.partition();
+                ZkUtils zkUtils = ZkUtils.apply(zkClient, false);
+                zkUtils.updatePersistentPath(offsetPath, o.toString(), zkUtils.DefaultAcls());
+            }
+            rdd.foreach(line -> System.out.println(line));
+        });
+
         return jssc;
     }
 
